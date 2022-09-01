0048dce598c99b3eb0ae46486bdcc67c9aa308b6
olivierlama <olivier.lama@gmail.com>
2022-09-01 11:34:40 +0200

    chargement todo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./styles.css";
+
+const ul = document.querySelector("ul");
+
+const todos = [
+  {
+    text: "je suis une todo",
+    done: false,
+  },
+  {
+    text: "Faire du javascript",
+    done: true,
+  },
+];
+
+function displayTodo() {
+  const todosNode = todos.map((todo, index) => {
+    //console.log(todo, index);
+    return createTodoElement(todo, index);
+  });
+  ul.innerHTML = "";
+  ul.append(...todosNode);
+}
+
+function createTodoElement(todo, index) {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <span class="todo ${todo.done ? "done" : ""}"></span>  
+    <p>${todo.text}</p>
+    <button>Supprimer</button>
+  `;
+  return li;
+}
+displayTodo();
